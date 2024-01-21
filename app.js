d9db6b1563a78fb162bbb9ba9a7806b7fbfda72a
vanditpopat <51928264+vanditpopat@users.noreply.github.com>
2024-01-21 18:04:54 -0500

    Final Commit before links added

--- app.js
+++ app.js
@@ -12,3 +12,31 @@ const opentab = (tabname, event) => {
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 };
+
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu(){
+    sidemenu.style.right = "0";
+}
+
+function closemenu(){
+    sidemenu.style.right = "-200px";
+}
+
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbwcvyR8cfi7n9OjHZqLX3TGMcFfuyV3id2-f7e0sF6opaRxePankFg3I5bYT6Qg0lwh/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message Sent successfully"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
